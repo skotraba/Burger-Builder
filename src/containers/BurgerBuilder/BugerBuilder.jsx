f59507b8e084afe7a290b6fbe0c3ddf3daa00133
skotraba <skotraba@gmail.com>
2020-12-09 10:59:14 -0500

    Added burger css and rendered it

--- src/containers/BurgerBuilder/BugerBuilder.jsx
+++ src/containers/BurgerBuilder/BugerBuilder.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import Auxx from '../hoc/Auxx';
+import Burger from '../../components/Burger/Burger';
 
 class BugerBuilder extends Component {
   render() {
     return (
      <Auxx>
-       <div>Burger</div>
+       <Burger/>
        <div>Build Controls</div>
      </Auxx>
     );
