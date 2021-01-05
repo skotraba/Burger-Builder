e2c174a32ae4052a59aa1ea12b80bfc58f47ed41
skotraba <skotraba@gmail.com>
2021-01-05 11:18:41 -0500

    Added remove button

--- src/containers/BurgerBuilder/BugerBuilder.jsx
+++ src/containers/BurgerBuilder/BugerBuilder.jsx
@@ -37,7 +37,19 @@ class BugerBuilder extends Component {
  }
 
  removeIngredientHandler = (type) => {
-
+  const oldCount = this.state.ingredients[type];
+  if(oldCount <= 0){
+    return;
+  }
+  const updatedCount = oldCount - 1;
+  const updatedIngredients = {
+    ...this.state.ingredients
+  };
+  updatedIngredients[type] = updatedCount;
+  const priceDeduction = INGREDIENT_PRICES[type];
+  const oldPrice = this.state.totalPrice;
+  const newPrice = oldPrice - priceDeduction;
+  this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
  }
   
   
@@ -46,7 +58,8 @@ class BugerBuilder extends Component {
      <Auxx>
        <Burger ingredients={this.state.ingredients}/>
        <BuildControls
-        ingredientAdded={this.addIngredientHandler}/>
+        ingredientAdded={this.addIngredientHandler}
+        ingredientRemove={this.removeIngredientHandler}/>
      </Auxx>
     );
   }
